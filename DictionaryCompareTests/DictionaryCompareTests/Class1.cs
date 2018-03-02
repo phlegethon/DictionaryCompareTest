08524cb8f07457b7336318d1eb4dd23d8a03070a
John-Paul Smith <github@phlegethon.co.uk>
2018-03-02 13:48:51 +0000

    First Version

---
+++ DictionaryCompareTests/DictionaryCompareTests/Class1.cs
@@ -0,0 +1,40 @@
+﻿using KellermanSoftware.CompareNetObjects;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace DictionaryCompareTests
+{
+    public class Class1 : Dictionary<string, string>
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public override bool Equals(object value)
+        {
+            bool returnValue;
+            CompareLogic compareLogic;
+            ComparisonResult result;
+
+            try
+            {
+                compareLogic = new CompareLogic();
+                result = compareLogic.Compare(this, value);
+
+                returnValue = result.AreEqual;
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+                Debugger.Break();
+                throw;
+            }
+
+            return returnValue;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ DictionaryCompareTests/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using DictionaryCompareTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// test that comparing BaseObjects (of the same type) works ok
+        /// </summary>
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 object1;
+            Class1 object2;
+            bool result;
+
+            object1 = new Class1();
+            object2 = new Class1();
+
+            // the two objects will have different Id's, so the compare should fail
+            result = object1.Equals(object2);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
